b9b597695e0d8035c8b84693a02025f5d11af795
Karishni Mehta <karishni25@gmail.com>
2020-09-22 12:23:25 +0530

    Updates

---
+++ precache-manifest.90c4db04f034057e2b7fb48e5dded32f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c4aa24eba572df1cb7f3f7fb5dc72fa",
+    "url": "/recipe-search/index.html"
+  },
+  {
+    "revision": "193a52cb8d306b587dea",
+    "url": "/recipe-search/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "f5f3bb33e4047f1dcc9f",
+    "url": "/recipe-search/static/js/2.08a43043.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/recipe-search/static/js/2.08a43043.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "193a52cb8d306b587dea",
+    "url": "/recipe-search/static/js/main.e70872e0.chunk.js"
+  },
+  {
+    "revision": "577da895b49789884271",
+    "url": "/recipe-search/static/js/runtime-main.627dc049.js"
+  }
+]);
